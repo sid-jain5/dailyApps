026116c1d98b555fdd02bd394ddac40be8aac5a0
SIDDHANT JAIN <sidjainsid05@gmail.com>
2020-04-12 11:41:29 +0530

    final try push back end

--- dailyApps-backend/src/main/java/com/personal/dailyApps/DailyAppsApplication.java
+++ dailyApps-backend/src/main/java/com/personal/dailyApps/DailyAppsApplication.java
@@ -9,6 +9,7 @@ public class DailyAppsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DailyAppsApplication.class, args);
 		System.out.println("DailyApps Application Running in background");
+		// Trying final push back end
 	}
 
 }
