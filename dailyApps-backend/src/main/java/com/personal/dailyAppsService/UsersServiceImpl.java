70a2e42c6b12450f97657f874de06467e23f4462
SIDDHANT JAIN <sidjainsid05@gmail.com>
2020-05-04 16:54:00 +0530

    changed methods in service class and added messages for delete func

--- dailyApps-backend/src/main/java/com/personal/dailyAppsService/UsersServiceImpl.java
+++ dailyApps-backend/src/main/java/com/personal/dailyAppsService/UsersServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.personal.dailyAppsDAO.UsersDAO;
 import com.personal.dailyAppsModel.Users;
+import com.personal.dailyAppsUtility.HashingUtility;
 
 public class UsersServiceImpl implements UsersService{
 	
@@ -15,29 +16,82 @@ public class UsersServiceImpl implements UsersService{
 	@Override
 	public Users authenticateUser(String username, String password) throws Exception {
 		// TODO Auto-generated method stub
-		Users users=usersDAO.getUserByUsername(username);
-		if(users.getPassword()==password)
-		return users;
-		return null;
+		Users userFromDB=usersDAO.getUserByUsername(username);
+		
+		//Checking if the user exists in database or not
+		if(userFromDB == null) {
+			throw new Exception("UserService.USER_NOT_FOUND");
+		}
+		
+		//Checking if the password field is null or not
+		String passwordFromDB = userFromDB.getHashedPassword();
+		if(passwordFromDB==null) {
+			throw new Exception("UserService.NULL_PASSWORD");
+		}
+		
+		String hashPassword = HashingUtility.getHashValue(password); // finding the hash value of the entered password 
+		
+		//Matching if the entered password matches the password stored in database
+		if(!hashPassword.equals(passwordFromDB)){
+			throw new Exception("UserService.INVALID_PASSWORD");
+		}
+		
+		userFromDB.setPassword(null);
+		userFromDB.setHashedPassword(null);
+		return userFromDB;
 	}
 
 	@Override
 	public String addUser(Users user) throws Exception {
 		// TODO Auto-generated method stub
 		
-		return usersDAO.addUser(user);
+		Users userFromDB = usersDAO.getUserByUsername(user.getUsername());
+
+		if(userFromDB!=null) {
+			throw new Exception("UserService.USER_EXISTS");
+		}
+		
+		String hashedPassword = HashingUtility.getHashValue(user.getPassword());
+		user.setHashedPassword(hashedPassword);
+		
+		String usernameFromDAO = usersDAO.addUser(user);
+		
+		if(!usernameFromDAO.equals(user.getUsername())) {
+			throw new Exception("UserService.ERROR");
+		}
+		
+		return "UserService.SUCCESS_MESSAGE"+usernameFromDAO;
 	}
 
 	@Override
 	public List<String> listUsers() throws Exception {
 		// TODO Auto-generated method stub
-		return usersDAO.getUsernameList();
+		List<String> listOfUsers = usersDAO.getUsernameList();
+		if(listOfUsers==null || listOfUsers.isEmpty()) {
+			throw new Exception("UserService.NO_USERNAME_FOUND");
+		}
+		return listOfUsers;
 	}
 
 	@Override
-	public String removeUser(String username) throws Exception {
+	public String removeUser(String username, String password) throws Exception {
 		// TODO Auto-generated method stub
-		return usersDAO.deleteUser(username);
+		Users userFromDB=usersDAO.getUserByUsername(username);
+		//Checking if the user exists in database or not
+		if(userFromDB == null) {
+			throw new Exception("UserService.USER_NOT_FOUND");
+		}
+		//Checking if the password field is null or not
+		String passwordFromDB = userFromDB.getHashedPassword();
+		if(passwordFromDB==null) {
+			throw new Exception("UserService.NULL_PASSWORD");
+		}
+		String hashPassword = HashingUtility.getHashValue(password); // finding the hash value of the entered password 
+		
+		//Matching if the entered password matches the password stored in database
+		if(!hashPassword.equals(passwordFromDB)){
+			throw new Exception("UserService.INVALID_PASSWORD");
+		}	
+		return "UserService.DELETE_SUCCESS" + usersDAO.deleteUser(username);
 	}
-
 }
